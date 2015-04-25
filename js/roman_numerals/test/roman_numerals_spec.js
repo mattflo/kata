7d29c46ea2cb269744683f3d05cff2c43c133b38
Matt Florence <matt.florence@outpace.com>
2015-04-25 09:48:17 -0500

    50 and 100

--- js/roman_numerals/test/roman_numerals_spec.js
+++ js/roman_numerals/test/roman_numerals_spec.js
@@ -1,13 +1,17 @@
 var chai = require('chai');
 chai.should();
 
-var romanDigitToInt = function (romanDigitToInt) {
-  if (romanDigitToInt == 'i') {
+var romanDigitToInt = function (digit) {
+  if (digit == 'i') {
     return 1;
-  } else if (romanDigitToInt == 'v'){
+  } else if (digit == 'v'){
     return 5;
-  } else {
+  } else if (digit == 'x'){
     return 10;
+  } else if (digit == 'l'){
+    return 50;
+  } else {
+    return 100;
   };
 };
 
@@ -21,6 +25,12 @@ describe('converting roman numerals to integers',function(){
     });
     it('should interpret x', function(){
       romanDigitToInt('x').should.equal(10);
+    });
+    it('should interpret l', function(){
+      romanDigitToInt('l').should.equal(50);
+    });
+    it('should interpret c', function(){
+      romanDigitToInt('c').should.equal(100);
     });
   });
 });
